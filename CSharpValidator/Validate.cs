da5ead95cb365f27e8279e12eed4c9c035e2f64b
mat roberts <mat@moleseyhill.com>
2017-06-22 11:34:41 +0100

    Added simple beginings of a validator

---
+++ CSharpValidator/Validate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpValidator
+{
+    public class Validate
+    {
+        private readonly List<ValidationError> errors = new List<ValidationError>();
+
+        public void That(bool value, string message, string property = null)
+        {
+            if (value == false)
+            {
+                errors.Add(new ValidationError(message, property));
+            }
+        }
+        public void That(Func<bool> lambda, string message, string property = null)
+        {
+            if (lambda() == false)
+            {
+                errors.Add(new ValidationError(message, property));
+            }
+        }
+
+        public bool HasErrors => errors.Any();
+        public IReadOnlyList<ValidationError> Errors => errors;
+    }
+
+    public class ValidationError
+    {
+        public ValidationError(string message, string property)
+        {
+            Message = message;
+            Property = property;
+        }
+        public string Message { get; set; }
+        public string Property { get; set; }
+    }
+}
---
+++ CSharpValidator/ValidatorTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CSharpValidator
+{
+    [TestFixture]
+    public class ValidatorTests
+    {
+        /*var validate = new Validator();
+
+        validate.That(object.Propert, Is.GreathT().And..., "Must be greathan", "proper");
+var object = new object ();
+validate.That(() => object.Property,  Is.GreathT().And..., "Must be greathan");
+
+validate.ErroMessages();*/
+        public class TestObject
+        {
+            public string Name { get; set; }
+            public int Id { get; set; }
+            public bool Flag { get; set; }
+        }
+
+        [Test]
+        public void Validate_IfABoolArgumentIsTrue_NoErrorIsRaised()
+        {
+            // Arrange
+            var testObject = new TestObject() { Flag = true };
+
+            // Act
+            var validate = new Validate();
+            validate.That(testObject.Flag, "Flag should be true");
+
+            // Assert
+            Assert.That(validate.HasErrors, Is.False);
+        }
+
+        [Test]
+        public void Validate_IfABoolArgumentIsFalse_AnErrorIsRaised()
+        {
+            // Arrange
+            var testObject = new TestObject() { Flag = false };
+
+            // Act
+            var validate = new Validate();
+            validate.That(testObject.Flag, "Flag should be true");
+
+            // Assert
+            Assert.That(validate.HasErrors, Is.True);
+            Assert.That(validate.Errors[0].Message, Is.EqualTo("Flag should be true"));
+        }
+
+        [Test]
+        public void Validate_IfABoolLambdaIsTrue_NoErrorIsRaised()
+        {
+            // Arrange
+            var testObject = new TestObject() { Name = "woot"};
+
+            // Act
+            var validate = new Validate();
+            validate.That(() => testObject.Name=="woot", "Name should be woot");
+
+            // Assert
+            Assert.That(validate.HasErrors, Is.False);
+        }
+
+        [Test]
+        public void Validate_IfABoolLambdatIsFalse_AnErrorIsRaised()
+        {
+            // Arrange
+            var testObject = new TestObject() { Name = "wrong" };
+
+            // Act
+            var validate = new Validate();
+            validate.That(() => testObject.Name == "woot", "Name should be woot");
+
+            // Assert
+            Assert.That(validate.HasErrors, Is.True);
+            Assert.That(validate.Errors[0].Message, Is.EqualTo("Name should be woot"));
+        }
+    }
+}
